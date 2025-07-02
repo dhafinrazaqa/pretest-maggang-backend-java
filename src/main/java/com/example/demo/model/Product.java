fc1d42f71900dd478aa31fb2d7946a40d7847d4a
dhafinrazaqa <dhafin.razaqa12@gmail.com>
2025-07-02 21:40:19 +0700

    Implement Product CRUD

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer stock;
+}
